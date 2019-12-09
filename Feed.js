6c5b910d88d92c005f5f3ebbf681e4e72bb2836c
avikant wadhwa <avikant16@hotmail.com>
2019-12-09 18:42:50 +0530

    export_class

--- Feed.js
+++ Feed.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react'
 import { View,Image } from 'react-native'
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import {AppLoading } from "expo";
+import * as Font from 'expo-font'
+export default class CardImageExample extends Component {
 
-
+    
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+      }
+    
+    
 
 

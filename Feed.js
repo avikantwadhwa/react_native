fcec52a1b695f9f2d9489bf347a6bfbb4902a672
avikant wadhwa <avikant16@hotmail.com>
2019-12-09 18:42:17 +0530

    init_native

---
+++ Feed.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { View,Image } from 'react-native'
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import {AppLoading } from "expo";
+
+
+
+

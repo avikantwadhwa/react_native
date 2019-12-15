230ae3ff6c3a1bcffc7daaf622131e78497f6708
avikant wadhwa <avikant16@hotmail.com>
2019-12-15 22:19:10 +0530

    direct

--- UseCamera.js
+++ UseCamera.js
@@ -18,5 +18,17 @@ export default class CameraExample extends React.Component {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
       return <View />;
-    } 
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+             
 }

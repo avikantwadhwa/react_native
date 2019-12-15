ca3952bd0179769256bf56b06d7f64d834cde611
avikant wadhwa <avikant16@hotmail.com>
2019-12-15 22:18:11 +0530

    settings_camera

--- UseCamera.js
+++ UseCamera.js
@@ -3,4 +3,10 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
 
+  

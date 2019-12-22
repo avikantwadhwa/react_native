3e9f5757e4de403c9c833177923bc6c632a4e0e1
avikant wadhwa <avikant16@hotmail.com>
2019-12-22 23:59:43 +0530

    mobx_export

--- Usingmob.js
+++ Usingmob.js
@@ -1,4 +1,4 @@
-import {action,observable} from 'mobx';
+import {action,observable, computed} from 'mobx';
 import { setLightEstimationEnabled } from 'expo/build/AR';
 
 class ObservableStore{
@@ -8,6 +8,7 @@ class ObservableStore{
     @action listmethod(val){
         this.list.push(val)
     }
+    @computed get 
 }
 const observableStore = new ObservableStore();
 export default observableStore;

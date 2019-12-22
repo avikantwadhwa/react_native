51b81ecaaf75251dc0c697a7d17390640f4b745c
avikant wadhwa <avikant16@hotmail.com>
2019-12-22 23:58:22 +0530

    mobx_variables

--- Usingmob.js
+++ Usingmob.js
@@ -1,3 +1,11 @@
 import {action,observable} from 'mobx';
 import { setLightEstimationEnabled } from 'expo/build/AR';
 
+class ObservableStore{
+
+    @observable list=[]
+
+    @action listmethod(val){
+        this.list.push(val)
+    }
+}

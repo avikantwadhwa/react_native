c45bbe0925df39230b6040ff8494d0afb27e94d3
avikant wadhwa <avikant16@hotmail.com>
2020-01-15 23:40:42 +0530

    set_state

--- UseImage.js
+++ UseImage.js
@@ -5,3 +5,17 @@ import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 import { Dropdown } from 'react-native-material-dropdown';
 
+export default class ImagePickerExample extends React.Component {
+  state = {
+    image: null,
+    category:[{
+      value: 'Hospital',
+    }, {
+      value: 'Fire',
+    }, {
+      value: 'Road',
+    }]
+  };
+
+  
+}

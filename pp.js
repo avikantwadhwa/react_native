7a806ae1996b03776850da1958af49a3ec3fb3a8
avikant wadhwa <avikant16@hotmail.com>
2020-01-18 23:09:13 +0530

    'add_state'

---
+++ pp.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import observableStore from './Usingmob'
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import ImagePickerExample from './UseImage'
+import CameraExample from './UseCamera'
+import UseCamerMod from './UseCamerMod'
+import { Dropdown } from 'react-native-material-dropdown';
+import Feed1 from './Feed1'
+import * as firebase from "firebase";
+
+@observer
+class App extends Component {
+
+  state = {
+    image: null,
+    category:[{
+      value: 'Hospital',
+    }, {
+      value: 'Fire',
+    }, {
+      value: 'Road',
+    }]
+  };
+
+  componentDidMount()
+  {
+    const firebaseConfig = {
+      apiKey: " ",
+      authDomain: " ",
+      databaseURL: " ",
+      projectId: " ",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: " "
+    };
+  
+    firebase.initializeApp(firebaseConfig)
+  }
+
+  _handleclick()
+  {
+    observableStore.listmethod(1);
+    console.log(observableStore.list)
+  }
+
+ async  _handlesignup()
+  {
+
+    try{
+
+          await firebase.auth()
+              .createUserWithEmailAndPassword(email, pass);
+  
+          alert("Account created");
+  
+          // Navigate to the Home page, the user is auto logged in
+  
+      } catch (error) {
+        alert(error.toString())
+      }
+  
+  
+}
+
+
+  _handlelogin()
+  {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage)
+    });
+  }
+  
+  
+  render(){
+  return (
+    <View style={styles.container}>
+      <Text>Open up start working on your app!</Text>
+      <Button title="CLICK" onPress={()=>this._handlelogin()}></Button>
+      {
+        observableStore.list.map((v)=>{
+          return (
+            <View><Text>{v}</Text></View>
+          )
+        })
+      }
+      
+      
+      </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+   
+    
+  },
+});
+
+export default App

12191ff30eaa38bbc7a7036d1e42106c2b273ea6
avikant wadhwa <avikant16@hotmail.com>
2020-01-18 23:06:49 +0530

    'image_pick'

--- UseImage.js
+++ UseImage.js
@@ -37,4 +37,30 @@ export default class ImagePickerExample extends React.Component {
     );
   }
 
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
 }

0e200061512cab28ecc7451f56bde115b9e11d56
avikant wadhwa <avikant16@hotmail.com>
2020-01-21 22:59:16 +0530

    rt

--- Feed1.js
+++ Feed1.js
@@ -9,3 +9,28 @@ const image=require('./assets/a.jpg')
 import { Card ,Icon} from 'react-native-elements';
 //import Card
 
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card title="Local Modules"
+        image={image}>
+        {/*react-native-elements Card*/}
+          <Text style={styles.paragraph}>
+            This is a card from the react
+          </Text>
+          <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+        </Card>
+        
+        
+            
+      
+      </View>
+    );
+  }
+}
+
+

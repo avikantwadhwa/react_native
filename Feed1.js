b0b3b529f0400ddae1f53a030f2d109f8da19f17
avikant wadhwa <avikant16@hotmail.com>
2020-01-21 22:59:33 +0530

    rt

--- Feed1.js
+++ Feed1.js
@@ -33,4 +33,19 @@ export default class App extends React.Component {
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+});

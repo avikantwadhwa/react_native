3be9d8f388167885030c759f9c6f0872406aa43f
avikant wadhwa <avikant16@hotmail.com>
2020-01-22 23:11:29 +0530

    class

--- UseCamerMod.js
+++ UseCamerMod.js
@@ -1,4 +1,17 @@
 import React, {Component} from 'react'
 import { Text, View, Button,Image ,ScrollView} from 'react-native'
 import * as ImagePicker from 'expo-image-picker';
+export default class UseCamerMod extends Component {
 
+    state={
+        image:null
+    }
+
+    
+    
+    
+    render() {
+        
+        
+    }
+}
